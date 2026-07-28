346c2602a48e1e2485f6bc2b0faf205777d843d7
Rithambara Vyas <navneetvyas986@gmail.com>
2026-07-28 17:54:16 +0530

    Time: 28 ms (72.09%), Space: 80.7 MB (11.43%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        vector<int> freq(26, 0);
+
+        for (char c : s) {
+            freq[c - 'a']++;
+        }
+
+        string left = "";
+        string middle = "";
+
+        for (int i = 0; i < 26; i++) {
+            left += string(freq[i] / 2, char('a' + i));
+
+            if (freq[i] % 2 == 1) {
+                middle = char('a' + i);
+            }
+        }
+
+        string right = left;
+        reverse(right.begin(), right.end());
+
+        return left + middle + right;
+    }
+};
